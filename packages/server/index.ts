fd58f5d270979473b2e18ece4890c3551eff050a
Aaron Dale <aaron.james.dale@gmail.com>
2024-03-21 14:47:04 -0700

    resolve

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,4 +1,19 @@
+import * as Sentry from "@sentry/bun";
+
 import { TEST_VALUE } from "math";
+
+const env = process.env.NODE_ENV;
+const debug = process.env.NODE_ENV === "development";
+console.log(env);
+console.log(debug);
+console.log(process.env.GAMESERV_SENTRY_DSN);
+
+Sentry.init({
+    environment: env,
+    dsn: process.env.GAMESERV_SENTRY_DSN,
+    // Performance Monitoring
+    tracesSampleRate: 1.0, // Capture 100% of the transactions
+});
 
 const server = Bun.serve<{ authToken: string }>({
     port: 3000,
@@ -7,7 +22,7 @@ const server = Bun.serve<{ authToken: string }>({
         if (success) {
             return undefined;
         }
-        return new Response(`hello world???: ${TEST_VALUE}`);
+        return new Response(`hello world???: ${TEST_VALUE}:${env}`);
     },
     websocket: {
         async message(ws, message) {
@@ -22,5 +37,12 @@ const server = Bun.serve<{ authToken: string }>({
         },
     },
 });
+
 console.log(TEST_VALUE);
 console.log(`listening on ${server.hostname}:${server.port}`);
+
+try {
+    throw new Error("Sentry Bun test");
+} catch (e) {
+    Sentry.captureException(e);
+}
